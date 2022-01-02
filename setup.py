34eb9f85fa707ed1102adc3f9c4ad77d93449631
David <david.moedinger@ketzu.net>
2022-01-02 15:41:03 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", mode="r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+REQUIRED_PACKAGES = [
+    'hangul_utils',
+    'git+https://github.com/ketzu/g2pk.git'
+    # 'g2pK' not sure how to handle alternative g2pK-kiwipiepy as I haven't published it
+    # but 'g2pK would work as well
+]
+
+setuptools.setup(
+    name="konoise",
+    version="0.1.1",
+    author="David Moedinger",
+    author_email="david.moedinger@ketzu.net",
+    description="konoise: Synthetic Sentence Noise Generator for Korean",
+    install_requires=REQUIRED_PACKAGES,
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ketzu/konoise",
+    packages=setuptools.find_packages(),
+    package_data={'konoise': []},
+    python_requires=">=3.6",
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
